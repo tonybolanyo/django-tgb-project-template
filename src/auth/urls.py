9b29eae243cc9bf8257113d4ad01b2f3f40c0738
Tony G. Bolaño <tonybolanyo@gmail.com>
2020-05-20 09:29:47 +0200

    feat: add simple auth forms

---
+++ src/auth/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+
+from .views import login_view, register_user
+
+
+urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('register/', register_user, name='register'),
+    path('logout/', LogoutView.as_view(), name='logout')
+]
---
+++ src/auth/views.py
@@ -0,0 +1,60 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.forms.utils import ErrorList
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.utils.translation import gettext as _
+
+from .forms import LoginForm, SignUpForm
+
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+
+    msg = None
+
+    if request.method == 'POST':
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                msg = _('Invalid credentials')
+        else:
+            msg = _('Error validating the form')
+
+    return render(request, 'accounts/login.html', {'form': form, 'msg': msg})
+
+
+def register_user(request):
+
+    msg = None
+    success = False
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+
+            msg = _('User created.')
+            success = True
+
+            # return redirect('/login/')
+
+        else:
+            msg = _('Form is not valid')
+    else:
+        form = SignUpForm()
+
+    return render(
+        request,
+        'accounts/register.html',
+        {'form': form, 'msg': msg, 'success': success},
+    )
